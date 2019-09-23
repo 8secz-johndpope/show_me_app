4f2456cdd47b085694d672e1dbf0befbd1e88a9d
服部明諭 <thunaotoko@gmail.com>
2019-09-23 15:49:57 +0900

    signup,signin機能の作成。およびsigninしたら初期画面に飛ぶ

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKzVOULbKvuvzptVXO-Wt7vz5gvbtohXI",
+  authDomain: "webproject-ef5ac.firebaseapp.com",
+  databaseURL: "https://webproject-ef5ac.firebaseio.com",
+  projectId: "webproject-ef5ac",
+  storageBucket: "",
+  messagingSenderId: "823336720817",
+  appId: "1:823336720817:web:cef9b067ab597054f64590"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
